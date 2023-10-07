752732cadf183076e9beb5c95974c7e1c0837939
PrajwalNikam <nprajwal342@gmail.com>
2023-10-07 22:15:08 +0530

    Input

---
+++ InputOutput/InputOutput.py
@@ -0,0 +1,15 @@
+
+
+
+x = 10;
+
+y = 20;
+
+print(x + y);
+
+a = input("Enter X:")
+b = input("Enter y:")
+
+print(a+b)
+
+
---
+++ InputOutput/InputOutput2.py
@@ -0,0 +1,6 @@
+
+
+a = int(input("Enter the value of a :"))
+b = int(input("Enter the value of b :"))
+
+print(a+b)
